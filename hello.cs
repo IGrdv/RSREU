4fab7805eca431e2ce6439bd08b779fc871c3cc3
Igor <IgorGrdv@yandex.ru>
2022-09-14 01:20:14 +0300

    Added C# project files

--- hello.cs
+++ hello.cs
@@ -1,10 +1,1 @@
-﻿namespace HelloWorld
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿Console.WriteLine("Hello, World!");
