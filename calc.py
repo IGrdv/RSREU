8033267889c8894418ff7e0d7f6b84633cfddf71
Igor <IgorGrdv@yandex.ru>
2022-09-14 17:27:33 +0300

    test: add exception handling
    Add exception handling for these types of errors:
    - Division by zero
    - Missing parentheses
    - Not enough / too many operators
    - Invalid expression

--- calc.py
+++ calc.py
@@ -6,13 +6,13 @@ def check_precedence(i):
     if i == '*':
         return 2
     if i == '/':
-    	return 2
+        return 2
     if i == '+':
         return 1
     if i == '-':
-    	return 1
+        return 1
     if i == '(':
-    	return 0
+        return 0
 
 
 def to_postfix(expression):
@@ -27,6 +27,9 @@ def to_postfix(expression):
             while op_stack[-1] != "(":
                 last = op_stack.pop()
                 queue.append(last)
+                if not op_stack:
+                    print('Mismatched parentheses.')
+                    exit(1)
             op_stack.pop()
         elif i in ('+', '-', '/', '*'):
             while op_stack and check_precedence(op_stack[-1]) >= check_precedence(i) and op_stack[-1] != "(":
@@ -42,7 +45,11 @@ def calc(postfix):
     for value in postfix:
         if value in ['-', '+', '*', '/']:
             op1 = stack.pop()
-            op2 = stack.pop()
+            try:
+                op2 = stack.pop()
+            except IndexError:
+                print('Invalid expression.')
+                exit(1)
             if value == '-':
                 stack.append(op2 - op1)
             if value == '+':
@@ -50,13 +57,33 @@ def calc(postfix):
             if value == '*':
                 stack.append(op2 * op1)
             if value == '/':
+                if op1 == 0:
+                    print('Division by zero')
+                    exit(1)
                 stack.append(op2 / op1)
         else:
-            stack.append(Decimal(value))
+            try:
+                stack.append(Decimal(value))
+            except InvalidOperation:
+                print('Invalid expression.')
+                exit(1)
     return stack.pop()
 
 
 formula = input('> ')
 tokenized_formula = re.findall(r"[\d.]+|[()+\-*/]", formula)
+operands_amount = 0
+operators_amount = 0
+for token in tokenized_formula:
+    if token in ('-', '+', '*', '/'):
+        operators_amount += 1
+    if token.replace('.', '', 1).isdigit():
+        operands_amount += 1
+if operators_amount < operands_amount - 1:
+    print('Not enough operators.')
+    exit(1)
+if operators_amount > operands_amount - 1:
+    print('Too many operators.')
+    exit(1)
 postfix_expression = to_postfix(tokenized_formula)
 print(calc(postfix_expression))
